b06f9d5939ef51adb789185a1e3dd9b2c313e9a8
Golden-Jaguar <104539538+Golden-Jaguar@users.noreply.github.com>
2022-04-27 18:07:39 -0700

    Add files via upload

---
+++ DatabaseInjectionExample.py
@@ -0,0 +1,86 @@
+import psycopg2
+from psycopg2 import sql
+import logging
+import sys
+connection = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="1111", #!!!!!! change to your password
+)
+connection.set_session(autocommit=True)
+
+with connection.cursor() as cursor:
+    cursor.execute('SELECT COUNT(*) FROM users')
+    result = cursor.fetchone()
+    print(result)
+
+def is_admin(username: str) -> bool:
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT
+                admin
+            FROM
+                users
+            WHERE
+                username = %(username)s
+        """,{
+            'username':username
+        })
+        result = cursor.fetchone()
+
+    if result is None:
+        #User does not exist
+        return False
+    else:
+        admin, = result
+        return admin
+
+def count_rows(table_name: str, limit: int) -> int:
+    logging.basicConfig(filename='countofrowslogged.log', level=logging.INFO)
+    logging.info('Started')
+    with connection.cursor() as cursor:
+        stmt = sql.SQL("""
+            SELECT
+                COUNT(*)
+            FROM (
+                SELECT
+                    1
+                FROM
+                    {table_name}
+                LIMIT
+                    {limit}
+            ) AS limit_query
+        """).format(
+            table_name=sql.Identifier(table_name),
+            limit=sql.Literal(limit),
+        )
+        try:
+            cursor.execute(stmt)
+        except:
+            logging.info('Call Error %s - Unable to Finalize or Recognize Value', sys.exc_info()[0])
+            return "Error occured, Check Log File."
+        result = cursor.fetchone()
+    logging.info(rowcount)
+    logging.info('Complete')
+    rowcount, = result
+    return rowcount
+
+x = str(input("Enter User in Database: "))
+userAdmin = is_admin(x)
+if userAdmin== True:
+        print("User is an Admin")
+elif userAdmin== False:
+        print("User is not an Admin")
+else:
+        print ("Error")
+#print(is_admin('test')) #!!!!!change to your username
+#print(is_admin("'; select true; --"))
+#print(count_rows('users',1))
+#print(count_rows('users',10))
+#print( count_rows("(select 1) as foo; update users set admin = true where name = 'haki'; --", 1))
+#print(is_admin("'; update users set admin = 'true' where username = 'alex'; select true; --"))
+#print(is_admin("alex"))
+
+# print(is_admin("'; update users set admin = 'true' where username = 'yan'; select true; --"))
+# print(is_admin("yan"))
